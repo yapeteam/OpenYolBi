fb99a788001161f9910245304e1b4cb8e7db3c0d
TIMER_err <zqq2333@outlook.com>
2024-09-17 21:39:09 +0800

    eagle

--- Core-1.18.1/src/cn/yapeteam/yolbi/module/impl/movement/Eagle.java
+++ Core-1.18.1/src/cn/yapeteam/yolbi/module/impl/movement/Eagle.java
@@ -1,26 +1,20 @@
 package cn.yapeteam.yolbi.module.impl.movement;
 
+import cn.yapeteam.loader.Natives;
 import cn.yapeteam.yolbi.event.Listener;
 import cn.yapeteam.yolbi.event.impl.player.EventUpdate;
 import cn.yapeteam.yolbi.module.Module;
 import cn.yapeteam.yolbi.module.ModuleCategory;
-import cn.yapeteam.yolbi.module.values.impl.BooleanValue;
+import cn.yapeteam.yolbi.utils.misc.VirtualKeyBoard;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.AirBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.HitResult.Type;
 
 public class Eagle extends Module {
-    private final BooleanValue autoBuild = new BooleanValue("AutoBuild", true);
-
     public Eagle() {
-        super("Eagle", ModuleCategory.MOVEMENT, 71);
-        addValues(autoBuild);
+        super("Eagle", ModuleCategory.MOVEMENT);
     }
 
     public static Block getBlock(BlockPos pos) {
@@ -37,36 +31,20 @@ public class Eagle extends Module {
     @Listener
     public void onUpdate(EventUpdate event) {
         if (mc.player != null && getBlockUnderPlayer(mc.player) instanceof AirBlock) {
-            if (mc.player.isOnGround()) {
+            if (mc.player.isOnGround())
                 KeyMapping.set(mc.options.keyShift.getKey(), true);
-                if (mc.player.getMainHandItem().getItem() instanceof BlockItem
-                        || mc.player.getOffhandItem().getItem() instanceof BlockItem && this.autoBuild.getValue()) {
-                    BlockHitResult hitResult = (BlockHitResult) mc.hitResult;
-                    if (hitResult != null && hitResult.getType() == Type.BLOCK) {
-                        BlockPos blockPos = hitResult.getBlockPos().relative(hitResult.getDirection());
-                        if (mc.level != null) {
-                            BlockState state = mc.level.getBlockState(blockPos);
-                            if (state.getBlock() instanceof AirBlock) {
-                                KeyMapping.set(mc.options.keyUse.getKey(), true);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        } else if (!Natives.IsKeyDown(VirtualKeyBoard.VK_LSHIFT))
+            KeyMapping.set(mc.options.keyShift.getKey(), false);
     }
 
     @Override
     public void onEnable() {
-        if (mc.player != null) {
+        if (mc.player != null)
             mc.player.setShiftKeyDown(false);
-            KeyMapping.set(mc.options.keyUse.getKey(), false);
-        }
     }
 
     @Override
     public void onDisable() {
         KeyMapping.set(mc.options.keyShift.getKey(), false);
-        KeyMapping.set(mc.options.keyUse.getKey(), false);
     }
 }
