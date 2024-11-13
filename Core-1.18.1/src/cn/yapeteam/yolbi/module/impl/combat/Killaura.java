1fd432609f58800d517fd6176506f7b9ca0ffedd
BeiD.d <157435057+Tzsnb@users.noreply.github.com>
2024-11-13 22:31:47 +0800

    Update Killaura aim and find target
    Update Killaura aim and find target

--- Core-1.18.1/src/cn/yapeteam/yolbi/module/impl/combat/Killaura.java
+++ Core-1.18.1/src/cn/yapeteam/yolbi/module/impl/combat/Killaura.java
@@ -29,11 +29,12 @@ public class Killaura extends Module {
         super("Killaura",ModuleCategory.COMBAT,InputConstants.KEY_R);
         addValues(cpsValue, rangeValue);
     }
+    private  NumberValue<Double> aimrange = new NumberValue<Double>("aIMRANGE",4.5,3.1,7.1,0.1);
     private  NumberValue<Integer> cpsValue = new NumberValue<Integer>("CPS", 11, 1, 20, 1);
     private  NumberValue<Double> rangeValue = new NumberValue<Double>("Range", 2.65, 2.0, 6.0, 0.01);
     private  NumberValue<Integer> player = new NumberValue<Integer>("Player",1,0,1,1);
     private static LivingEntity target;
-    private final List<LivingEntity> targets = new ArrayList<>();
+    List<LivingEntity> targets = new ArrayList<>();
 
     @Override
     protected void onEnable() {
@@ -49,32 +50,42 @@ public class Killaura extends Module {
     public final boolean cheak(LivingEntity a){
         return !a.isDeadOrDying()&&!a.isInvisible()&&a!=mc.player;
     }
-    @Listener
-    public void oner(EventRender2D event) {
-        List<LivingEntity> targets = new ArrayList<>();
-        mc.gui.getChat().addMessage(new TextComponent("AAA3A"));
+    public final LivingEntity findtarget(){
+       targets.clear();
+        //  mc.gui.getChat().addMessage(new TextComponent("AAA3A"));
         for (Entity entity : mc.level.entitiesForRendering()) {
             if (entity instanceof LivingEntity) {
                 LivingEntity livingEntity = (LivingEntity)entity;
                 if (cheak(livingEntity)) {
-                    targets.add(livingEntity);
+                    return livingEntity;
                 }
             }
         }
+        return null;
+    }
+    @Listener
+    public void oner(EventRender2D event) {
+
 
         target = null;
+        if(targets.isEmpty()||target==null||target!=null&&unjztargetrange(target)>=rangeValue.getValue()){
+            target = findtarget();
+            if(target==null){
+                return;
+            }
+        }
         if (!targets.isEmpty()) {
-            mc.gui.getChat().addMessage(new TextComponent("AA32A"));
+            //mc.gui.getChat().addMessage(new TextComponent("AA32A"));
             target = targets.get(0);
         }
 
         if (target != null) {
-            mc.gui.getChat().addMessage(new TextComponent("AAA2A"));
+          // mc.gui.getChat().addMessage(new TextComponent("AAA2A"));
             float[] rotations = RotationUtils.getSimpleRotations(target);
-            mc.gui.getChat().addMessage(new TextComponent(String.valueOf(rotations[0])));
-            mc.gui.getChat().addMessage(new TextComponent(String.valueOf(rotations[1])));
-            mc.gui.getChat().addMessage(new TextComponent("AAA2A"));
-            mc.gui.getChat().addMessage(new TextComponent(target.getName().toString()));
+        //    mc.gui.getChat().addMessage(new TextComponent(String.valueOf(rotations[0])));
+         //   mc.gui.getChat().addMessage(new TextComponent(String.valueOf(rotations[1])));
+          //  mc.gui.getChat().addMessage(new TextComponent("AAA2A"));
+         //   mc.gui.getChat().addMessage(new TextComponent(target.getName().toString()));
             mc.player.setYRot(rotations[0]);
             mc.player.setXRot(rotations[1]);
         }
@@ -82,5 +93,6 @@ public class Killaura extends Module {
 
            // mc.getConnection().send(ServerboundInteractPacket.createAttackPacket(target, mc.player.isShiftKeyDown()));
           //  mc.player.swing(InteractionHand.MAIN_HAND);
-
+public final double jztargetrange(LivingEntity a){return Math.abs(a.getX()-mc.player.getX()) + Math.abs(a.getZ()-mc.player.getZ()) + Math.abs(a.getY()-mc.player.getY());}
+public final double unjztargetrange(LivingEntity a){return Math.abs(a.getX()-mc.player.getX()) + Math.abs(a.getZ()-mc.player.getZ()) ;}
 }
